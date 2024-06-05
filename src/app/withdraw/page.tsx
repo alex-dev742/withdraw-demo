311ab7a442397df6f2b9e7d25a2f20ab34589b14
Alex <alex@pchoice.co.za>
2024-06-05 15:50:24 +0200

    feat: Add links and transitions to withdrawal methods on the Withdraw page

--- src/app/withdraw/page.tsx
+++ src/app/withdraw/page.tsx
@@ -28,35 +28,53 @@ export default function Withdraw() {
       <div className="">
         <h2 className="text-xl font-bold mb-2">Withdraw Methods</h2>
         <div className="grid grid-cols-3 gap-4 mt-6">
-          <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center">
-            <img src="/ozow.webp" alt="Ozow" className="h-12" />
-          </div>
-          <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center">
-            <img src="/bluvoucher.webp" alt="BluVoucher" className="h-12" />
-          </div>
-          <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center">
-            <img src="/1voucher.webp" alt="1Voucher" className="h-12" />
-          </div>
-          <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center">
-            <img src="/easyload.webp" alt="Easyload" className="h-12" />
-          </div>
-          <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center">
-            <img src="/dpovisa.webp" alt="DPO by Visa" className="h-12" />
-          </div>
-          <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center">
-            <img src="/dpozapper.webp" alt="DPO Zapper" className="h-12" />
-          </div>
-          <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center">
-            <img src="/dposnapscan.webp" alt="DPO Snapscan" className="h-12" />
-          </div>
+          <Link href="/ozow-withdraw">
+            <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center transition duration-300 ease-in-out hover:bg-zinc-600">
+              <img src="/ozow.webp" alt="Ozow" className="h-12" />
+            </div>
+          </Link>
+          <Link href="/bluvoucher-withdraw">
+            <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center transition duration-300 ease-in-out hover:bg-zinc-600">
+              <img src="/bluvoucher.webp" alt="BluVoucher" className="h-12" />
+            </div>
+          </Link>
+          <Link href="/1voucher-withdraw">
+            <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center transition duration-300 ease-in-out hover:bg-zinc-600">
+              <img src="/1voucher.webp" alt="1Voucher" className="h-12" />
+            </div>
+          </Link>
+          <Link href="/easyload-withdraw">
+            <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center transition duration-300 ease-in-out hover:bg-zinc-600">
+              <img src="/easyload.webp" alt="Easyload" className="h-12" />
+            </div>
+          </Link>
+          <Link href="/dpovisa-withdraw">
+            <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center transition duration-300 ease-in-out hover:bg-zinc-600">
+              <img src="/dpovisa.webp" alt="DPO by Visa" className="h-12" />
+            </div>
+          </Link>
+          <Link href="/dpozapper-withdraw">
+            <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center transition duration-300 ease-in-out hover:bg-zinc-600">
+              <img src="/dpozapper.webp" alt="DPO Zapper" className="h-12" />
+            </div>
+          </Link>
+          <Link href="/dposnapscan-withdraw">
+            <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center transition duration-300 ease-in-out hover:bg-zinc-600">
+              <img
+                src="/dposnapscan.webp"
+                alt="DPO Snapscan"
+                className="h-12"
+              />
+            </div>
+          </Link>
           <Link href="/celbux-withdraw">
-            <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center">
+            <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center transition duration-300 ease-in-out hover:bg-zinc-600">
               <img src="/celbux.png" alt="Celbux" className="h-12" />
             </div>
           </Link>
           <Link href="/blubux-withdraw">
-            <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center">
-              <img src="/blubux.png" alt="Celbux" className="h-12" />
+            <div className="p-4 bg-zinc-700 rounded-lg flex items-center justify-center transition duration-300 ease-in-out hover:bg-zinc-600">
+              <img src="/blubux.png" alt="Blubux" className="h-12" />
             </div>
           </Link>
         </div>
